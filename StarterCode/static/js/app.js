d74dd66a99b4b9eca3f2a7bd62fb2a1613970327
Allison Howe <allisonhowe77@gmail.com>
2020-10-27 23:18:38 -0400

    bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,18 +4,31 @@ d3.json("samples.json").then(function(bellybuttondata) {
   // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
   console.log(bellybuttondata)
   var trace1 = {
-    x: bellybuttondata.samples[0].otu_ids.slice(0, 9).map(x => `OTU ${x}`),
-    y: bellybuttondata.samples[0].sample_values.slice(0, 9),
-    type: "bar"
+    y: bellybuttondata.samples[0].otu_ids.slice(0, 9).reverse().map(x => `OTU ${x}`),
+    x: bellybuttondata.samples[0].sample_values.slice(0, 9).reverse(),
+    type: "bar",
+    orientation: 'h',
+    text: bellybuttondata.samples[0].otu_labels.slice(0, 9).reverse(),
   };
   
   var data = [trace1];
   
-  var layout = {
-    title: "'Bar' Chart"
+  Plotly.newPlot("bar", data, {});
+
+  var trace1 = {
+    x: bellybuttondata.samples[0].otu_ids,
+    y: bellybuttondata.samples[0].sample_values,
+    mode: 'markers',
+    text: bellybuttondata.samples[0].otu_labels,
+    marker: {
+      size: bellybuttondata.samples[0].sample_values,
+      color: bellybuttondata.samples[0].otu_ids
+    }
   };
   
-  Plotly.newPlot("bar", data, layout);
+  var data = [trace1];
+  
+  Plotly.newPlot('bubble', data, {});
 
   //   // Use D3 to select the dropdown menu
   //   var dropdownMenu = d3.select("#selDataset");
